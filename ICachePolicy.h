3787decb3d8029465ae97e08d525732208b89506
FelixYuanSy <ysy12141314@gmail.com>
2026-01-20 19:36:55 +0800

    编写抽象类

---
+++ ICachePolicy.h
@@ -0,0 +1,12 @@
+#include <iostream>
+
+namespace MeltiCache
+{
+    template <typename Key,typename Value>
+    class ICachePolicy
+    {
+        virtual void put(Key key,Value value) = 0;
+        virtual void get(Key key,Value& value) = 0;
+        virtual Value get(Key key) = 0;
+    };
+}
